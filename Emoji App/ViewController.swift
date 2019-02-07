f29688101abb44ec7a3bb9f254d7318587b35d1d
EricCacciavillani <EricCacciavillani@github.com>
2019-02-06 21:50:50 -0500

    Initial Commit

---
+++ Emoji App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Emoji App
+//
+//  Created by Eric Cacciavillani on 2/6/19.
+//  Copyright © 2019 Awesome. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
